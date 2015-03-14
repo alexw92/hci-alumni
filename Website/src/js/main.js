a959d4d799f91b00bf265301b1ae83e0b918391e
Armin <inm-ascaron@gmx.de>
2015-03-14 22:11:29 +0100

    first attempt to use sqlite database

--- Website/src/js/main.js
+++ Website/src/js/main.js
@@ -1,8 +1,10 @@
 /* define global app context */
 var app = {
 	router: new Router(),
+	sqldb: new DatabaseController(),
 };
 
 jQuery(function () {
 	app.router.run();
+	//app.sqldb.testfunct();
 });
