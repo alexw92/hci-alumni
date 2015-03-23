b045461259f7b804c04e8ad793084a3ab7d1fd06
JayBeeCee <Benedikt.Pfaff@web.de>
2015-03-23 11:39:19 +0100

    registration form update

--- Website/src/js/controllers/RegistrationController.js
+++ Website/src/js/controllers/RegistrationController.js
@@ -3,21 +3,76 @@ var RegistrationController = function () {
 
 	this.exampleFunction();
 	this.initValidation();
-	this.setSubmitbutton();
+	this.bindEvents();
 };
 
 RegistrationController.prototype.exampleFunction = function() {
 	console.log(this.TAG + 'constructor called');
 };
 
-RegistrationController.prototype.setSubmitbutton = function() {
+RegistrationController.prototype.bindEvents = function() {
+	var self = this;
 	console.log(this.TAG + 'submit');
-	$("#btnSubmitReg").on('click', function(event){
+	$('#btnSubmitReg').on('click', function(event){
 		event.preventDefault();
 		console.log('SubmitBtn clicked');
 		$('#registrationForm').formValidation('validate');
 	});
+	$('#noMemberRadio').on('change', function(event) {
+		console.log('membership selected');
+		$('#membershipData').fadeOut('slow');
+		$('#membershipData').formValidation('destroy');
+	});
+	$('#memberRadio').on('change', function(event) {
+		console.log('membership selected');
+		$('#membershipData').fadeIn();
+		self.initMembershipValidation();
+		$('#membershipData').formValidation('validate');
+	});
 };
+
+RegistrationController.prototype.initMembershipValidation = function () {
+	$('#registrationForm').formValidation({
+        framework: 'bootstrap',
+        icon: 
+		{
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        err: 
+		{
+            // You can set it to popover
+            // The message then will be shown in Bootstrap popover
+            container: 'tooltip'
+        },
+        fields: 
+		{
+            firstName: 
+			{
+
+                validators: 
+				{
+                    notEmpty: 
+					{
+                        message: 'Bitte tragen Sie Ihren Vornamen ein'
+                    }
+                }
+            },
+            lastName: 
+			{
+                validators: 
+				{
+                    notEmpty: 
+					{
+                        message: 'Bitte tragen Sie Ihren Nachnamen ein'
+                    }
+                }
+            }
+		}
+	});
+};
+
 
 RegistrationController.prototype.initValidation = function () {
 	$('#registrationForm').formValidation({
@@ -36,6 +91,28 @@ RegistrationController.prototype.initValidation = function () {
         },
         fields: 
 		{
+			title:
+			{
+				validators:
+				{
+					notEmpty:
+					{
+						message: 'Bitte geben Sie eine Anrede an'
+					},
+					callback: 
+					{
+						message: 'Bitte wählen Sie eine Anrede aus',
+						callback: function(value, validator, $field)
+						{
+								if(value == 'Anrede')
+								{
+									return false;
+								}
+								return true;
+						}
+					}
+				}
+			},
             firstName: 
 			{
 
