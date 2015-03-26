9fa118e2109d3eb9d8c9d990891574f22b9d3a2f
Thomas Handwerker <handwerker.thomas@gmail.com>
2015-03-26 21:54:22 +0100

    implement navigation logic; hide elements which are only visible for registered user

---
+++ Website/src/js/utils/Navigation.js
@@ -0,0 +1,35 @@
+var Navigation = (function () {
+	var _nav = $('.navigation:first'),
+		_hideableNavIds = [
+			'nav-mentoring',
+			'nav-publication',
+			'nav-search'
+		];
+
+	/**
+	 * private methods
+	 */
+	 var showNavigationItems = function () {
+	 	$.each(_hideableNavIds, function (index, navItem) {
+	 		$(_nav).find('#' + navItem).removeClass('hidden');
+	 	});
+	 };
+
+	 var hideNavigationItems = function () {
+	 	$.each(_hideableNavIds, function (index, navItem) {
+	 		$(_nav).find('#' + navItem).addClass('hidden');
+	 	});
+	 };
+
+	/**
+	 * public methods
+	 */
+	return {
+		update: function () {
+			if(Session.isSessionActive())
+				showNavigationItems();
+			else
+				hideNavigationItems();
+		}
+	};
+})();
